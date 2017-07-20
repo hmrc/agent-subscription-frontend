3b2f1af946b67ce5d5f5d3cb6bc551e72818a48a
john.revill <john.revill@digital.hmrc.gov.uk>
2017-07-20 10:00:34 +0100

    [APB-991][JR,RV,AO] redirect to the new sos jounery for agents and preserve the known facts in the mongodb

--- it/uk/gov/hmrc/agentsubscriptionfrontend/controllers/StartControllerISpec.scala
+++ it/uk/gov/hmrc/agentsubscriptionfrontend/controllers/StartControllerISpec.scala
@@ -1,14 +1,21 @@
 package uk.gov.hmrc.agentsubscriptionfrontend.controllers
 
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.mvc.Result
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{contentType, _}
+import uk.gov.hmrc.agentmtdidentifiers.model.Utr
+import uk.gov.hmrc.agentsubscriptionfrontend.models.KnownFactsResult
+import uk.gov.hmrc.agentsubscriptionfrontend.repository.KnownFactsResultMongoRepository
 import uk.gov.hmrc.agentsubscriptionfrontend.stubs.AuthStub
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class StartControllerISpec extends BaseControllerISpec {
 
   private lazy val controller: StartController = app.injector.instanceOf[StartController]
   private lazy val configuredGovernmentGatewayUrl = "http://configured-government-gateway.gov.uk/"
+  private lazy val repo = app.injector.instanceOf[KnownFactsResultMongoRepository]
 
   override protected def appBuilder: GuiceApplicationBuilder = super.appBuilder
     .configure("government-gateway.url" -> configuredGovernmentGatewayUrl)
@@ -56,7 +63,7 @@ class StartControllerISpec extends BaseControllerISpec {
       val result = await(controller.showNonAgentNextSteps(authenticatedRequest()))
 
       status(result) shouldBe 200
-      bodyOf(result) should include("/signed-out")
+      bodyOf(result) should include("/redirect-to-sos")
     }
 
     "redirect to the company-auth-frontend sign-in page if the current user is not logged in" in {
@@ -73,11 +80,49 @@ class StartControllerISpec extends BaseControllerISpec {
   }
 
   "returnAfterGGCredsCreated" should {
-    "redirect to check-agency-status page" in {
+    "redirect to the subscription-details page if given a valid KnownFactsResult ID" in {
+      val knownFactsResult = KnownFactsResult(Utr("9876543210"), "AA11AA", "Test organisation name", isSubscribedToAgentServices = true)
+      val persistedId = await(repo.create(knownFactsResult))
+
+      val result = await(controller.returnAfterGGCredsCreated(FakeRequest(GET, "?id=" + persistedId)))
+
+      status(result) shouldBe 303
+      redirectLocation(result).head should include ("/subscription-details")
+    }
+
+    "redirect to the check-agency-status page if given an invalid KnownFactsResult ID" in {
+      val knownFactsResult = KnownFactsResult(Utr("9876543210"), "AA11AA", "Test organisation name", isSubscribedToAgentServices = true)
+      val persistedId = await(repo.create(knownFactsResult))
+
+      val result = await(controller.returnAfterGGCredsCreated(FakeRequest(GET, s"?id=A$persistedId")))
+
+      status(result) shouldBe 303
+      redirectLocation(result).head should include ("/check-agency-status")
+    }
+
+    "redirect to check-agency-status page if there is no valid KnownFactsResult ID" in {
       val result = await(controller.returnAfterGGCredsCreated(FakeRequest()))
 
       status(result) shouldBe 303
       redirectLocation(result).head should include ("/check-agency-status")
+    }
+
+    "delete the persisted KnownFactsResult if given a valid KnownFactsResult ID" in {
+      val knownFactsResult = KnownFactsResult(Utr("9876543210"), "AA11AA", "Test organisation name", isSubscribedToAgentServices = true)
+      val persistedId = await(repo.create(knownFactsResult))
+
+      await(controller.returnAfterGGCredsCreated(FakeRequest(GET, "?id=" + persistedId)))
+
+      await(repo.findKnownFactsResult(persistedId)) shouldBe None
+    }
+
+    "repopulate the KnownFacts session store with the persisted KnownFactsResult, if given a valid KnownFactsResult ID" in {
+      val knownFactsResult = KnownFactsResult(Utr("9876543210"), "AA11AA", "Test organisation name", isSubscribedToAgentServices = true)
+      val persistedId = await(repo.create(knownFactsResult))
+
+      await(controller.returnAfterGGCredsCreated(FakeRequest(GET, "?id=" + persistedId)))
+
+      sessionStoreService.knownFactsResult shouldBe Some(knownFactsResult)
     }
   }
 }
